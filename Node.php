691cf1122ebcc585eab3d3f7ed02eb10704e2f30
multifinger <Marina_oss@list.ru>
2022-02-03 15:46:32 +0400

    node init

---
+++ Node.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Node implements NodeInterface
+{
+
+    public function construct(string $name)
+    {
+    }
+
+    public function toString(): string
+    {
+        // TODO: Implement toString() method.
+    }
+
+    public function getName(): string
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function getChildren(): array
+    {
+        // TODO: Implement getChildren() method.
+    }
+
+    public function addChild(Node $node): NodeInterface
+    {
+        // TODO: Implement addChild() method.
+    }
+}
